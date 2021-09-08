990464916c9fe2358463344c10410731121b5f17
Dmitry Podrez <dima.padrez.00@icloud.com>
2021-09-08 20:31:36 +0300

    feat: add task dream

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,22 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+if(Array.isArray(members)){
+ let r = members.filter(member => typeof member === 'string');
+ let y = [];
+ r.forEach((item)=>{
+ let t = item.replace(/\s+/g,'');
+ y.push(t);
+ });
+ let z1 = [];
+ y.forEach((item)=>{
+ let t = item.charAt(0).toUpperCase();
+ z1.push(t);
+ })
+ let z2 = z1.sort().join('');
+ return z2;
+ }
+return false;
 }
+
