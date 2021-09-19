dcc352f82d0f1bd6c93e27955bd01b4642657744
Dmitry Podrez <dima.padrez.00@icloud.com>
2021-09-19 17:33:24 +0300

    feat: add new solution

--- src/what-season.js
+++ src/what-season.js
@@ -11,14 +11,25 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(date) {  
-  if (!date) return 'Unable to determine the time of year!';
-  if (Object.prototype.toString.call(date) !== '[object Date]') {
-  throw new Error("Invalid date!");
-  }
-  const mon = date.getMonth() + 1;
-  if (mon> 0 && mon < 3 || mon === 12) return 'winter';
-  if (mon > 2 && mon < 6) return 'spring';
-  if (mon > 5 && mon < 9) return 'summer';
-  if (mon> 8 && mon < 12) return 'autumn';
+export default function getSeason(date) {
+if (!date){ 
+return 'Unable to determine the time of year!';
+}
+try {
+date.getTime();
+} catch (error) {
+throw new Error ('Invalid date!');
+}
+let month = date.getMonth();
+if (month >= 2 && month <= 4){ 
+  return 'spring';
+}
+else if (month >= 5 && month <= 7){
+return 'summer';
+}
+else if (month >= 8 && month <= 10) {
+  return 'autumn';
+}
+else 
+return 'winter';
 }
