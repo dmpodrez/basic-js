4dce702f8fbea86a016c74d5c8d77868814d8086
Dmitry Podrez <dima.padrez.00@icloud.com>
2021-09-19 16:49:06 +0300

    feat: add solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,25 +4,30 @@ import { NotImplementedError } from '../extensions/index.js';
  * Implement chainMaker object according to task description
  * 
  */
-export default {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ export default {
+x:[],
+getLength() {
+return this.x.length;
+},
+addLink(value) {
+this.x.push(`( ${value} )`);
+return this;
+},
+removeLink(position) {
+if (position < 1 || position > this.x.length - 1 || typeof position !== 'number') {
+this.x = [];
+throw new Error("You can't remove incorrect link!");
+}
+this.x.splice(position - 1, 1)
+return this;
+},
+reverseChain() {
+this.x.reverse();
+return this;
+},
+finishChain() {
+let y = `${this.x.join('~~')}`;
+this.x = [];
+return y;
   }
 };
